9d0927e31e06e62664c935ccc36289115f06cda3
PIGSx <hiury1315ajac@gmail.com>
2025-07-21 15:38:40 -0300

    criando primeiras impressoaoes sobre o menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
 
 function App() {
   return <>
+ <Header/>
  <Login/>
   </>
 }
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,3 +1,4 @@
+
 import './Login.scss'
 import React, { useState } from "react";
 import { FaUser, FaLock, FaEnvelope } from "react-icons/fa";
